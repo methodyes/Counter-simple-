d7d74c828686f092cfaf41228456611a5a8bd7c5
metho <metholk@gmail.com>
2019-03-02 20:15:54 +0530

    initial count

--- simpleapp/src/Compo/Counter.js
+++ simpleapp/src/Compo/Counter.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 
  class Counter extends Component {
+     constructor(props) {
+       super(props)
+     
+       this.state = {
+          counter : 00000000
+       }
+     }
+     
   render() {
     return (
       <div>
